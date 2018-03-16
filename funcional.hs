70ff0e6a8f0e809690b7ad95994d659454ba157b
byWhish <lareugearrdo@gmail.com>
2018-03-16 09:03:10 -0300

    hasta el 22

--- funcional.hs
+++ funcional.hs
@@ -1,52 +1,130 @@
 --1)
-id ::  a -> a
-id x = x
+id' ::  a -> a
+id' x = x
 
 --2)
-const :: a -> b -> a
-const x _ = x
+const' :: a -> b -> a
+const' x _ = x
 
 --3)
-fst :: (a,b) -> a
-fst (x,y) = x
+fst' :: (a,b) -> a
+fst' (x,_) = x
 
 --4)
-snd :: (a,b) -> b
-snd (x,y) = Main.fst ( swap (x,y) )
+snd' :: (a,b) -> b
+snd' (_,y) = y
 
 --5)
-swap :: (a,b) -> (b,a)
-swap (x,y) = (y,x)
+swap' :: (a,b) -> (b,a)
+swap' (x,y) = (y,x)
 
---6)
-head :: [a] -> a
-head (x:xs) = x
+--6) no considero la lista vacia
+head' :: [a] -> a
+head' (x:xs) = x
 
---7)
-tail :: [a] -> [a]
-tail (x:xs) = xs
+--7) no considero la lista vacia
+tail' :: [a] -> [a]
+tail' (x:xs) = xs
 
 --8)
-sum, product :: Num a => [a] -> a
-sum [] = 0
-sum (x:xs) = x + Main.sum xs
+sum', product' :: Num a => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
 
---9)
-product [] = 1
-product (x:xs) = x * Main.product xs
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+--09)
+elem', noElem'  :: Eq a => a -> [a] -> Bool
+elem' x [] = False
+elem' x (y:ys) | x == y = True
+               | otherwise = elem' x ys  
+
+noElem' x [] = True
+noElem' x (y:ys) | x == y = False
+                 | otherwise = noElem' x ys
 
 --10)
-elem, noElem  :: Eq a => a -> [a] -> Bool
-elem x [] = False
-elem x (y:ys) | x == y = True
-              | otherwise = Main.elem x ys  
+and', or' :: [Bool] -> Bool
+and' [] = True
+and' (x:xs) = x && and' xs
 
---11)
-noElem x [] = True
-noElem x (y:ys) | x == y = False
-                | otherwise = noElem x ys
+or' [] = False
+or' (x:xs) = x || or' xs
+
+--11) no considero la lista vacia
+last' :: [a] -> a
+last' [x] = x
+last' (x:xs) = last' xs
 
 --12)
-subset :: Eq a => [a] -> [a] -> Bool
-subset [] _ = True
-subset (x:xs) (y:ys) = x == y && subset xs ys
+init' :: [a] ->  [a]
+init' [] = []
+init' [x] = []
+init' (x:xs) = x : init' xs
+
+--13)
+subset' :: Eq a => [a] -> [a] -> Bool
+subset' [] _ = True
+subset' (x:xs) (y:ys) = x == y && subset' xs ys
+
+--14)
+(+++) ::  [a] -> [a] -> [a]
+(+++) [][] = []
+(+++) (x:xs) y = x : (+++) xs y
+(+++) x (y:ys) = y : (+++) x ys
+
+--15)
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (x:xs) = x +++ concat' xs
+
+--16) no considero la lista vacia
+--lista indexada a partir de 0
+(!!!) :: [a] -> Int -> a
+(!!!) (x:xs) 0 = x
+(!!!) (x:xs) i = (!!!) xs (i-1)
+
+--17) considero que pido menor o igual cantidad de elementos de lista
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' i (x:xs) = x : take' (i-1) xs
+
+--18) consideto que tiro igual o menor cantidad de elementos que la lista
+drop' :: Int -> [a] -> [a]
+drop' 1 (x:xs) = xs
+drop' i (x:xs) = drop' (i-1) xs
+
+--19) asumo listas de igual cantidad de elementos
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [][] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+--20) asumo minimo 2 elementos
+splitAt' :: Int -> [a] -> ([a],[a])
+splitAt' i xs = ( take i xs, drop i xs)
+
+--21) asumo minimo un elemento 
+maximun', minimun' :: Ord a => [a] -> a
+maximun' [x] = x 
+maximun' (x:xs) | x > ( head' xs ) = maximun' ( x : ( tail' xs ) )
+                | otherwise = maximun' xs
+
+minimun' [x] = x 
+minimun' (x:xs) | x < ( head' xs ) = minimun' ( x : ( tail' xs ) )
+                | otherwise = minimun' xs  
+
+--22)
+lookup' :: Eq a => a -> [(a,b)] -> Maybe b
+lookup' y [] = Nothing
+lookup' y (x:xs) | y == fst' x = snd' x
+                 | otherwise 
+                
+--23)                 
+unzip' :: [(a,b)] -> ([a],[b])
+unzip' [] = []
+unzip' (x:xs) = ( fst' x : ys, snd' x : zs ) 
+                where ys = []
+                      zs = []    
+                
+
